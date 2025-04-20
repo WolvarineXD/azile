cc9ca4f9b1511a522a2ee91267865aa2bee467f9
WolvarineXD <148878451+WolvarineXD@users.noreply.github.com>
2025-04-20 18:35:56 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,22 +24,42 @@ const Footer = () => {
               </p>
               
               <div className="flex space-x-4 mt-6">
-                <a href="https://www.facebook.com/" className="bg-theme-gray hover:bg-theme-orange text-white p-2 rounded-full transition-colors">
-                  <Facebook size={18} />
-                </a>
-                <a href="https://x.com/i/flow/single_sign_on" className="bg-theme-gray hover:bg-theme-orange text-white p-2 rounded-full transition-colors">
-                  <Twitter size={18} />
-                </a>
-                <a href="https://in.linkedin.com/" className="bg-theme-gray hover:bg-theme-orange text-white p-2 rounded-full transition-colors">
-                  <Linkedin size={18} />
-                </a>
-                <a href="https://www.instagram.com/accounts/login/?hl=en" className="bg-theme-gray hover:bg-theme-orange text-white p-2 rounded-full transition-colors">
-                  <Instagram size={18} />
-                </a>
-                <a href="https://www.youtube.com/@bcnishantreddy" className="bg-theme-gray hover:bg-theme-orange text-white p-2 rounded-full transition-colors">
-                  <Youtube size={18} />
-                </a>
-              </div>
+  <a
+    href="https://www.facebook.com/"
+    aria-label="Visit our Facebook page"
+    className="bg-theme-gray hover:bg-theme-orange text-white p-2 rounded-full transition-colors"
+  >
+    <Facebook size={18} />
+  </a>
+  <a
+    href="https://x.com/i/flow/single_sign_on"
+    aria-label="Visit our Twitter page"
+    className="bg-theme-gray hover:bg-theme-orange text-white p-2 rounded-full transition-colors"
+  >
+    <Twitter size={18} />
+  </a>
+  <a
+    href="https://in.linkedin.com/"
+    aria-label="Visit our LinkedIn page"
+    className="bg-theme-gray hover:bg-theme-orange text-white p-2 rounded-full transition-colors"
+  >
+    <Linkedin size={18} />
+  </a>
+  <a
+    href="https://www.instagram.com/accounts/login/?hl=en"
+    aria-label="Visit our Instagram page"
+    className="bg-theme-gray hover:bg-theme-orange text-white p-2 rounded-full transition-colors"
+  >
+    <Instagram size={18} />
+  </a>
+  <a
+    href="https://www.youtube.com/@bcnishantreddy"
+    aria-label="Visit our YouTube channel"
+    className="bg-theme-gray hover:bg-theme-orange text-white p-2 rounded-full transition-colors"
+  >
+    <Youtube size={18} />
+  </a>
+</div>
             </div>
             
             <div>
